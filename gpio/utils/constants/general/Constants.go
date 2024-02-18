ba02f65ae4b896b4730d64b34d3f6329d0ff150e
Luis D. Nuñez <ldnunez@live.com>
2024-02-18 13:27:53 -0600

    Add comments

--- gpio/utils/constants/general/Constants.go
+++ gpio/utils/constants/general/Constants.go
@@ -1,5 +1,15 @@
+// Package general defines constants used in the gpio module.
+//
+// Author: Luis D. Nuñez V. (daniel38192)
+//
+// The general package includes one main constant:
+//
+// PathToGpioBase: This constant represents the path to the GPIO base in the sysfs interface.
+//
+// This constant is used by the functions in the kernelutils package to interact with the sysfs interface for GPIOs.
 package general
 
 const (
+	// PathToGpioBase represents the path to the GPIO base in the sysfs interface.
 	PathToGpioBase = "/sys/class/gpio/"
 )
